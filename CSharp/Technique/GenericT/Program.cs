5169dea945a0fb1bbed906fee503e3bd12067a9f
foolishsunday <foolishsunday@users.noreply.github.com>
2020-12-26 17:50:22 +0800

    泛型T的使用，遍历类的属性
    泛型T的使用，遍历类的属性

---
+++ CSharp/Technique/GenericT/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericT
+{
+    class Program
+    {
+        /// <summary>
+        /// 本实例代码演示了如何利用泛型T，遍历任意类的所有属性
+        /// </summary>
+        /// <param name="args"></param>
+        private static string Property2String<T>(T model)
+        {
+            //获取实例的type
+            Type t = model.GetType();
+
+            //把所有属性转化为list
+            var list = t.GetProperties().ToList();
+            string text = string.Empty;
+
+            //遍历属性
+            list.ForEach(x =>
+            {
+                object value = x.GetValue(model);
+
+                //属性值和属性名转为字符串
+                var str = string.Format(" [{0}={1}] ", x.Name, value.ToString());
+                text += str;
+            });
+            return text;
+        }
+        static void Main(string[] args)
+        {
+            //使用方法
+
+            //遍历Student类的所有属性
+            var text = Property2String<Student>(new Student() { Name = "Levi", Id = "abcd", Age = 18 });
+            Console.WriteLine(text);
+
+            //遍历School类的所有属性
+            text = Property2String<School>(new School() { Name = "college", Address = "beijing", ClassCount = 99 });
+            Console.WriteLine(text);
+            Console.Read();
+        }
+    }
+
+    //纯属性的类
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public int Age { get; set; }
+    }
+
+    //纯属性的类
+    public class School
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int ClassCount { get; set; }
+    }
+}
--- CSharp/Technique/IOC_App/Program.cs
+++ CSharp/Technique/IOC_App/Program.cs
@@ -7,41 +7,39 @@ using System.Threading.Tasks;
 
 namespace IOC_App
 {
-    public static class SpecifyInfoHandler
+    public static class DispatchHandler
     {
-        public static CKeyValuePairCollection DownloadSuccessResult(GetSpecifyInfoEventArgs e)
+        public static void DownloadSuccessResult(DispatchEventArgs e)
         {
-            var resultData = new CKeyValuePairCollection();
-            resultData.Add(new CKeyValuePairInfo() { Key = "cim", Value = "Auto" });
+   
             var data = e.ResultData.First();
             Console.WriteLine("key = {0}, value = {1}",data.Key, data.Value);
             Console.WriteLine("name = {0}, id = {1}", e.Name, e.Id);
-            return resultData;
         }
     }
     class Program
     {       
         static void Main(string[] args)
         {
-            var data = new GetSpecifyInfoEventArgs();
+            var data = new DispatchEventArgs();
             data.Command = "DownloadSuccessResult";
             data.Id = System.Guid.NewGuid().ToString();
-            data.Name = "Levi";
+            data.Name = "Factory";
 
-            var collection = new CKeyValuePairCollection();
-            collection.Add(new CKeyValuePairInfo { Key = "Calc", Value = "1234" });
+            var collection = new KeyValueCollection();
+            collection.Add(new KeyValuePair { Key = "StoreName", Value = "LUL" });
             data.ResultData = collection;
             data.Type = "ClientRequest";
-            GetSpecifyInfo(null, data);
+            DispatchWork(null, data);
             Console.ReadKey();
         }
-        public static void GetSpecifyInfo(object sender, GetSpecifyInfoEventArgs e)
+        public static void DispatchWork(object sender, DispatchEventArgs e)
         {
             if (e.Type == "ClientRequest")
             {
                 try
                 {
-                    typeof(SpecifyInfoHandler).GetMethod(e.Command).Invoke(null, new object[] { e });
+                    typeof(DispatchHandler).GetMethod(e.Command).Invoke(null, new object[] { e });
                 }
                 catch (Exception ex)
                 {
@@ -52,30 +50,33 @@ namespace IOC_App
         }
          
     }
-    public class GetSpecifyInfoEventArgs : ReportStatusEventArgs<CKeyValuePairCollection>
-    { 
-        public string Type { get; set; }
-        public string Command { get; set; }
-        public CKeyValuePairCollection ResultData { get; set; }
+    public class BaseEventArgs : EventArgs
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+
     }
-    public class CKeyValuePairCollection : Collection<CKeyValuePairInfo>
-    { 
-        public CKeyValuePairInfo this[string key] { get { return this[key]; } }
-        //public bool ContainKey(string key) 
+    public class ReportMessageEventArgs<T> : BaseEventArgs
+    {
+        public T Data { get; set; }
     }
-    public class CKeyValuePairInfo
+    public class KeyValuePair
     {
         public string Key { get; set; }
         public string Value { get; set; }
     }
-    public class ReportStatusEventArgs : EventArgs
+    public class KeyValueCollection : Collection<KeyValuePair>
     {
-        public string Name { get; set; }
-        public string Id { get; set; }
-        
+        public KeyValuePair this[string key] { get { return this[key]; } }
+        //public bool ContainKey(string key) 
     }
-    public class ReportStatusEventArgs<T> : ReportStatusEventArgs
+    public class DispatchEventArgs : ReportMessageEventArgs<KeyValueCollection>
     { 
-        public T Data { get; set; }
+        public string Type { get; set; }
+        public string Command { get; set; }
+        public KeyValueCollection ResultData { get; set; }
     }
+
+
+
 }
