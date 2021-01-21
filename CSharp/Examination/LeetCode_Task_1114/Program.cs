6fbf4df67c701a360849d80407292fbeae23aa1c
foolishsunday <foolishsunday@users.noreply.github.com>
2021-01-22 01:32:22 +0800

    多线程习题
    多线程习题

---
+++ CSharp/Examination/LeetCode_Task_1114/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LeetCode_Task_1114
+{
+    public class Foo
+    {
+        Barrier barrier = new Barrier(1);
+        public Foo()
+        {
+            var task1 = new Task(() =>
+            {
+                First(() =>
+                {
+                    Console.Write("first");
+                    Thread.Sleep(1000);
+                });
+                barrier.RemoveParticipant();
+            });
+            var task2 = new Task(() =>
+            {
+                Task.WaitAll(task1);
+                barrier.AddParticipant();
+                Second(() =>
+                {
+                    Console.Write("second");
+                    Thread.Sleep(500);
+                });
+                barrier.RemoveParticipant();
+            });
+            var task3 = new Task(() =>
+            {
+                Task.WaitAll(task1, task2);
+                barrier.AddParticipant();
+                Third(() =>
+                {
+                    Console.Write("third");
+                });
+                barrier.RemoveParticipant();
+   
+            });
+
+            task1.Start();
+            task2.Start();
+            task3.Start();
+        }
+
+        public void First(Action printFirst)
+        {
+
+            // printFirst() outputs "first". Do not change or remove this line.
+            printFirst();
+
+        }
+
+        public void Second(Action printSecond)
+        {
+
+            // printSecond() outputs "second". Do not change or remove this line.
+            printSecond();
+        }
+
+        public void Third(Action printThird)
+        {
+
+            // printThird() outputs "third". Do not change or remove this line.
+            printThird();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Foo foo = new Foo();
+       
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSharp/Examination/LeetCode_Task_1115/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LeetCode_Task_1115
+{
+    /*
+     两个不同的线程将会共用一个 FooBar 实例。其中一个线程将会调用 foo() 方法，另一个线程将会调用 bar() 方法。
+
+    请设计修改程序，以确保 "foobar" 被输出 n 次。
+
+         */
+    class FooBar
+    {
+        private int n;
+
+        public AutoResetEvent fooSignal = new AutoResetEvent(false);
+        public AutoResetEvent barSignal = new AutoResetEvent(false);
+        public FooBar(int n)
+        {
+            this.n = n;
+            //Task.Run(() => foo());
+            //Task.Run(() => bar());
+            var ts1 = new Thread(() => Foo(()=> Console.Write("foo")));
+            var ts2 = new Thread(() => Bar(() => Console.Write("bar")));
+            ts1.Start();
+            ts2.Start();
+            fooSignal.Set();
+        }
+        public void Foo(Action printFoo)
+        {
+
+            for (int i = 0; i < n; i++)
+            {
+                fooSignal.WaitOne();
+                printFoo();
+                barSignal.Set();
+            }
+
+        }
+
+        public void Bar(Action printBar)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                barSignal.WaitOne();
+                printBar();
+                fooSignal.Set();
+
+            }
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fb = new FooBar(1000);
+
+            //var ts1 = new Thread(() => fb.foo()) ;
+            //var ts2 = new Thread(() => fb.bar());
+            //ts1.Start();
+            //ts2.Start();
+            Console.Read();
+        }
+    }
+}
